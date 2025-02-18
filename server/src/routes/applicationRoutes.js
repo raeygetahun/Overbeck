d96781640e7adf154468621f44613b7355f27921
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2025-02-18 20:13:22 +0100

    removed subdirectories for gh actions

--- server/src/routes/applicationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const applicationController = require('../controllers/applicationController');
-
-const router = express.Router();
-
-router.get('/approved-timeslots', applicationController.getApprovedApplications);
-router.get('/pending-timeslots', applicationController.getPendingApplications);
-
-module.exports = router;
--- server/src/services/firebaseService.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const config = require('../config');
-
-admin.initializeApp({
-  credential: admin.credential.cert(config.firebaseConfig.privateKey),
-  databaseURL: `https://${config.firebaseConfig.projectId}.firebaseio.com`,
-});
-
-module.exports = admin;
