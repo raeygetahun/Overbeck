270f2e2ac2b5074d7ed77f0f10a15c69ee1edbf8
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2025-02-18 22:19:31 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false,  }
+const nextConfig = {
+      basePath: '/Overbeck', // Replace with your repository name
+      assetPrefix: '/Overbeck/',
+      reactStrictMode: false,
+  }
 
 module.exports = nextConfig
